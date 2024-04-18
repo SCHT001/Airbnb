57d5094a198e0eb0d0265b1d8d534ae8dcbf5c80
SCHT001 <sachetsubedi001@gmail.com>
2024-04-18 16:53:47 +0545

    Layout adjustments

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import ItemCard from "@/components/dashboard/ItemCard";
 import FilterNavbar from "@/components/filterNavigation/FilterNavbar";
-import Header from "@/components/navbar/Header";
 import { useEffect, useState } from "react";
 
 export default function Home() {
@@ -18,7 +17,6 @@ export default function Home() {
 	return (
 		<div>
 			<div>
-				<Header></Header>
 				<FilterNavbar></FilterNavbar>
 				<ItemCard></ItemCard>
 			</div>
---
+++ apps/web/src/app/rooms/[room-id]/page.tsx
@@ -0,0 +1,5 @@
+const Room = () => {
+	return <div>Room1</div>;
+};
+
+export default Room;
