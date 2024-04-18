70b76ff40ae6309facd0d8bc27fbe08ef876cc53
SCHT001 <sachetsubedi001@gmail.com>
2024-04-18 23:55:16 +0545

    [api]:multer added

--- apps/api/src/routes/api.ts
+++ apps/api/src/routes/api.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
+import multer from "multer";
 import auth from "./auth/routes";
 import listings from "./listings/routes";
 
+const upload = multer({ dest: "uploads/" });
+
 const router: Router = Router();
+
 router.use("/auth", auth);
-router.use("/listings", listings);
+
+router.use("/listings", upload.single("avatar"), listings);
 export default router;
