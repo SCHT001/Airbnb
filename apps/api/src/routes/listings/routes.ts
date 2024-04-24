efd11fd95b763477fba25ee3db8f6ee822d6ede6
SCHT001 <sachetsubedi001@gmail.com>
2024-04-24 16:29:35 +0545

    [api]:express-fileupload

--- apps/api/src/routes/listings/routes.ts
+++ apps/api/src/routes/listings/routes.ts
@@ -1,13 +1,17 @@
 import { Router } from "express";
+import fileUpload from "express-fileupload";
 import { addListing, getListings } from "./controller";
 
 const router: Router = Router();
+
+router.use(fileUpload());
 
 router.get("/", (req, res) => {
 	return getListings(req, res);
 });
 
 router.post("/", (req, res) => {
+	console.log(req.files);
 	return addListing(req, res);
 });
 
