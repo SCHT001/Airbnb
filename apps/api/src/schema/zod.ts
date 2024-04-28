3034f079322607a477084e88f3b50266cfb9c539
Sachet Subedi <sachetsubedi001@gmail.com>
2024-04-28 19:07:10 +0545

    [db]:prisma schema changes (price strig to int)

--- apps/api/src/schema/zod.ts
+++ apps/api/src/schema/zod.ts
@@ -5,3 +5,12 @@ export const signInWithPhoneSchema = z.object({
   phone: z.string().min(9),
   countryCode: z.string().min(1),
 });
+
+export const listingSchema = z.object({
+  id: z.string().uuid(),
+  accommodation: z.string(),
+  title: z.string(),
+  description: z.string(),
+  host_id: z.string().uuid(),
+  price: z.string(),
+});
