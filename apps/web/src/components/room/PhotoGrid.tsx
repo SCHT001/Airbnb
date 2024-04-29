791c887eb051e206db4e6e88fa129374fcb3ae3e
Sachet Subedi <sachetsubedi001@gmail.com>
2024-04-29 18:22:30 +0545

    [web]:shad/cn calander ui adjustments

--- apps/web/src/components/room/PhotoGrid.tsx
+++ apps/web/src/components/room/PhotoGrid.tsx
@@ -2,28 +2,28 @@ import { cn } from "@/lib/utils";
 import { FC } from "react";
 
 const PhotoGrid: FC<{
-	photos: {
-		id: string;
-		listingId: string;
-		url: string;
-	}[];
+  photos: {
+    id: string;
+    listingId: string;
+    url: string;
+  }[];
 }> = ({ photos }) => {
-	// slice the photos array to only show the first 5 photos
-	// const slicedPhotos = photos.slice(0, 5);
-	console.log(photos);
-	return (
-		<div className="grid grid-cols-4 gap-2">
-			{photos.map((photo, i) => (
-				<div key={i} className={cn(i === 0 ? "col-span-2 row-span-2" : "")}>
-					<img
-						src={photo.url}
-						className={(cn(i === 0 ? "h-96" : ""), "rounded-xl w-full h-56")}
-						alt=""
-					/>
-				</div>
-			))}
-		</div>
-	);
+  // slice the photos array to only show the first 5 photos
+  // const slicedPhotos = photos.slice(0, 5);
+  console.log(photos);
+  return (
+    <div className="grid grid-cols-4 gap-2 pt-10">
+      {photos.map((photo, i) => (
+        <div key={i} className={cn(i === 0 ? "col-span-2 row-span-2" : "")}>
+          <img
+            src={photo.url}
+            className={(cn(i === 0 ? "h-96" : ""), "rounded-xl w-full h-full")}
+            alt=""
+          />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default PhotoGrid;
