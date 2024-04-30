ae9313f5fd315cba739ca0bfa972a558aacf19c2
Sachet Subedi <sachetsubedi001@gmail.com>
2024-04-30 20:46:45 +0545

    [web]:calander disabled fror booked days

--- apps/web/src/components/room/Booking.tsx
+++ apps/web/src/components/room/Booking.tsx
@@ -1,5 +1,7 @@
+"use client";
 import { A_booking } from "@/lib/axios";
 import { T_Room } from "@/types";
+import { useQuery } from "@tanstack/react-query";
 import { AxiosResponse } from "axios";
 import { FC, useEffect, useState } from "react";
 import { DateRange } from "react-day-picker";
@@ -12,6 +14,16 @@ import { Separator } from "../ui/separator";
 const Booking: FC<{
   roomData: T_Room;
 }> = ({ roomData }) => {
+  let bookedDates: Date[] = [];
+  // query for already booked dates
+  const { isSuccess, data } = useQuery({
+    queryKey: ["bookedDates"],
+    queryFn: async () => {
+      const response = await A_booking.get(`/${roomData.id}`);
+      return response.data;
+    },
+  });
+
   const [range, setRange] = useState<DateRange | undefined>();
 
   //  count the no of days selected
@@ -30,12 +42,10 @@ const Booking: FC<{
   };
 
   useEffect(() => {
-    console.log(range);
     daysCount();
   }, [range]);
 
   const bookPlace = async () => {
-    // console.log("clicked");
     try {
       const response: AxiosResponse = await A_booking.post("/add", {
         listing_id: roomData.id,
@@ -43,6 +53,9 @@ const Booking: FC<{
       });
 
       if (response.data) {
+        setTimeout(() => {
+          location.reload();
+        }, 300);
         return toast.success("Room booked successfully");
       }
     } catch (e) {
@@ -50,39 +63,53 @@ const Booking: FC<{
     }
   };
 
-  return (
-    <Card className="mb-5 shadow-xl">
-      <CardHeader>
-        <CardTitle>
-          {roomData.price} <span className="text-sm text-slate-500">night</span>{" "}
-        </CardTitle>
-      </CardHeader>
-      <CardContent>
-        <Calendar
-          min={4}
-          mode="range"
-          selected={range}
-          onSelect={setRange}
-          className="rounded-md border"
-        />
-        <Button onClick={bookPlace} className="w-full mt-5">
-          Reserve
-        </Button>
+  if (isSuccess) {
+    const bookedDates = data.bookings.map(
+      (date: { date: Date; id: string; listing_id: string; isBooked: any }) => {
+        return new Date(date.date);
+      }
+    );
+    // console.log(bookedDates);
+    return (
+      <Card className="mb-5 shadow-xl">
+        <CardHeader>
+          <CardTitle>
+            {roomData.price}{" "}
+            <span className="text-sm text-slate-500">night</span>{" "}
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Calendar
+            min={4}
+            mode="range"
+            selected={range}
+            onSelect={setRange}
+            className="rounded-md border"
+            disabled={bookedDates}
+          />
+          <Button onClick={bookPlace} className="w-full mt-5">
+            Reserve
+          </Button>
 
-        <div className="priceDetails mt-5 flex flex-col gap-5">
-          <p className=" ">
-            ${roomData.price} x {daysCount()} nights{" "}
-            <span className="float-right">${roomData.price * daysCount()}</span>
-          </p>
-          <Separator />
-          <p className="font-bold">
-            Total{" "}
-            <span className="float-right">${roomData.price * daysCount()}</span>
-          </p>
-        </div>
-      </CardContent>
-    </Card>
-  );
+          <div className="priceDetails mt-5 flex flex-col gap-5">
+            <p className=" ">
+              ${roomData.price} x {daysCount()} nights{" "}
+              <span className="float-right">
+                ${roomData.price * daysCount()}
+              </span>
+            </p>
+            <Separator />
+            <p className="font-bold">
+              Total{" "}
+              <span className="float-right">
+                ${roomData.price * daysCount()}
+              </span>
+            </p>
+          </div>
+        </CardContent>
+      </Card>
+    );
+  }
 };
 
 export default Booking;
