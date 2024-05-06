f319703c2d5dc397cd2070c6df17a444fd13ee10
Sachet Subedi <sachetsubedi001@gmail.com>
2024-05-06 15:25:25 +0545

    [web]: logout api implemented

--- apps/web/src/components/navbar/Logo.tsx
+++ apps/web/src/components/navbar/Logo.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Logo = () => {
-	return (
-		<div className=" flex items-center">
-			<Image src={"/airbnb.png"} width={100} height={500} alt="Logo"></Image>
-		</div>
-	);
+  return (
+    <div className=" flex items-center">
+      <Link href={"/"}>
+        <Image src={"/airbnb.png"} width={100} height={500} alt="Logo"></Image>
+      </Link>
+    </div>
+  );
 };
 
 export default Logo;
