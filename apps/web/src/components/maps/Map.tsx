f23196dae9b9ef31c379f3b17fe7994feb64d13e
Sachet Subedi <sachetsubedi001@gmail.com>
2024-05-09 18:58:08 +0545

    [web]:Draggable marker

--- apps/web/src/components/maps/Map.tsx
+++ apps/web/src/components/maps/Map.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Icon } from "leaflet";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
 const Map = () => {
   const costomIcon = new Icon({
     iconUrl: "/location.png",
@@ -19,11 +19,7 @@ const Map = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]} icon={costomIcon}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        <Marker position={[51.505, -0.09]} icon={costomIcon}></Marker>
       </MapContainer>
     </div>
   );
